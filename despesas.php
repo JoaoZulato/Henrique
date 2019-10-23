56a480601558de4925d390d75b363b42d361e779
JoaoZulato <56926650+JoaoZulato@users.noreply.github.com>
2019-10-23 11:58:29 -0300

    Add files via upload

---
+++ despesas.php
@@ -0,0 +1,25 @@
+<?php
+$mes[12], $TAno, $TMes, $Variacao, $DIFreal;
+$principal,$bonus,$outros,$secundario,$impostos,$servicos,$planejado;
+
+
+
+
+function progressao ($mes[],$total,$planejado){
+
+
+}
+
+
+
+function comparacao ($mes[],$total,$secundario,$principal,$outros,$bonus){
+
+}
+
+function comparacao2 ($mes[],$principal,$bonus,$outros,$secundario,$impostos,$servicos){
+	$i=0;
+
+	}
+}
+
+?>
---
+++ rec.php
@@ -0,0 +1,28 @@
+<?php
+$mes[12], $TAno, $TMes, $Variacao, $DIFMes;
+$principal,$bonus,$outros,$secundario,$impostos,$servicos,$planejado;
+
+
+
+
+function progressao ($mes[],$total,$planejado){
+
+
+}
+
+
+
+function comparacao ($mes[],$total,$secundario,$principal,$outros,$bonus){
+
+}
+
+function dados ($mes[],$principal,$bonus,$outros,$secundario,$impostos,$servicos){
+	$i=0;
+	$total[12];
+	while(i<12){
+	total[i] = mes[] + principal + bonus + outros + secundario + impostos + servicos;
+	i++;
+	}
+}
+
+?>
